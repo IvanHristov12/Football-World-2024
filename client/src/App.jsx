a97dbdfdb28b39d56cae398f65088ebfe5d4a0bd
Ivan Hristov <145547332+IvanHristov12@users.noreply.github.com>
2024-08-04 15:33:55 +0300

    Added Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
     return (
-        <>
 
-        </>
+        <div className="bg-white">
+            <Header />
+            <Home />
+        </div>
     )
 }
 
