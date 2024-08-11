49d43c37909dd1761b359250078ec686f5f7d28b
Ivan Hristov <145547332+IvanHristov12@users.noreply.github.com>
2024-08-12 01:32:17 +0300

    Fix ContactUs form for authorized users

--- client/src/components/aboutus/AboutUs.jsx
+++ client/src/components/aboutus/AboutUs.jsx
@@ -1,12 +1,18 @@
+import { useContext } from "react";
 import ContactUs from "./ContactUs";
 import FAQ from "./FAQ";
 import OurTeam from "./OurTeam";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export default function AboutUs() {
+    const { isAuthenticated } = useContext(AuthContext)
+
     return (
     <>
         <OurTeam />
+        {isAuthenticated &&
         <ContactUs /> 
+        }
         <FAQ />
     </>
 );
