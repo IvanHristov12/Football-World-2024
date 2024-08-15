d4d839e526e5a85085a50b67dce09caffee82b69
Ivan Hristov <145547332+IvanHristov12@users.noreply.github.com>
2024-08-15 03:25:09 +0300

    Fix logout

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -32,8 +32,8 @@ export const useLogout = () => {
     const { logout: localLogout } = useAuthContext();
 
     const logoutHandler = async () => {
-        localLogout();
         await logout();
+        localLogout();
     };
 
     return logoutHandler
